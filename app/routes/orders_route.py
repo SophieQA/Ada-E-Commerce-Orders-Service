112a72087b81658928511cac0c84858e97eb7b4a
Mikelle <mmikelle.wade@adadevelopersacademy.org>
2026-04-15 14:24:44 -0400

    decouples route utils with HTTP response logic

--- app/routes/orders_route.py
+++ app/routes/orders_route.py
@@ -1,7 +1,7 @@
 from ..db import db
 from ..models.order import Order
-from flask import Blueprint, request, Response
-from .route_utilities import create_order, get_models_with_filters, validate_model, update_model
+from flask import Blueprint, request, Response, abort, make_response
+from ..utilities import create_order, get_models_with_filters, validate_model, update_model
 
 bp = Blueprint("orders_blueprint", __name__, url_prefix="/orders")
 
@@ -10,7 +10,12 @@ bp = Blueprint("orders_blueprint", __name__, url_prefix="/orders")
 def post_order():
     request_body = request.get_json()
 
-    return create_order(Order, request_body)
+    try:
+        new_order = create_order(Order, request_body)
+    except KeyError as e:
+        abort(make_response({"message": f"Invalid: Missing key ({e.args[0]})"}, 400))
+
+    return new_order.to_dict(), 201
 
 
 @bp.get("/")
@@ -20,22 +25,38 @@ def get_all_orders():
 
 @bp.get("/<id>")
 def get_single_order(id):
-    order = validate_model(Order, id)
+    try:
+        order = validate_model(Order, id)
+    except ValueError as e:
+        abort(make_response({"message": str(e)}, 400))
+    except LookupError as e:
+        abort(make_response({"message": str(e)}, 404))
 
     return order.to_dict()
 
 
 @bp.put("/<id>")
 def update_order(id):
-    order = validate_model(Order, id)
-    request_body = request.get_json()
+    try:
+        order = validate_model(Order, id)
+    except ValueError as e:
+        abort(make_response({"message": str(e)}, 400))
+    except LookupError as e:
+        abort(make_response({"message": str(e)}, 404))
 
-    return update_model(order, request_body)
+    update_model(order, request.get_json())
+
+    return Response(status=204, mimetype="application/json")
 
 
 @bp.delete("/<id>")
 def delete_order(id):
-    order = validate_model(Order, id)
+    try:
+        order = validate_model(Order, id)
+    except ValueError as e:
+        abort(make_response({"message": str(e)}, 400))
+    except LookupError as e:
+        abort(make_response({"message": str(e)}, 404))
 
     db.session.delete(order)
     db.session.commit()
